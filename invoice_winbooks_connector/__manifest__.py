9aba1d1274e494c24126aa5c3af8fd60e7abc019
AH-Makdad-Elavi <ahmed.makdad@elavi.agency>
2025-09-19 09:38:44 +0100

    first push

---
+++ invoice_winbooks_connector/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'invoice_winbooks_connector',
+    'version': '18.0.1.0.0',
+    'depends': ['base','account','mail'],
+    'data': [
+        'views/res_config_settings_views.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
---
+++ invoice_winbooks_connector/models/__init__.py
@@ -0,0 +1,3 @@
+from . import partner
+from . import account_move_send
+from . import res_config_settings
