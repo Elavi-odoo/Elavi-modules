d0e634d0293a0bceb00c725ad4961841c748ccca
AH-Makdad-Elavi <ahmed.makdad@elavi.agency>
2025-09-23 17:29:28 +0100

    add button send to winbooks

--- invoice_winbooks_connector/__manifest__.py
+++ invoice_winbooks_connector/__manifest__.py
@@ -1,12 +1,13 @@
 {
-    'name': 'invoice_winbooks_connector',
+    'name': 'invoice winbooks connector',
     'version': '18.0.1.0.0',
     'depends': ['base','account','mail'],
     'data': [
         'views/res_config_settings_views.xml',
         'views/account_journal_views.xml',
-        'views/action_send_winbooks_email.xml',
+        'views/account_move_views.xml',
     ],
     'installable': True,
     'application': True,
+    'license': 'LGPL-3',
 }
--- invoice_winbooks_connector/models/account_move.py
+++ invoice_winbooks_connector/models/account_move.py
@@ -1,37 +1,37 @@
 from odoo import models, _
 from odoo.exceptions import UserError
-import base64
+
 
 class AccountMove(models.Model):
     _inherit = "account.move"
 
     def action_send_winbooks_email(self):
+        global winbooks_email
         for move in self:
             journal = move.journal_id
             winbooks_email = journal.WinBooks_email
             if not winbooks_email:
                 raise UserError(_("No WinBooks email configured on the journal %s.") % journal.display_name)
 
-            pdf_content = self.env['ir.actions.report']._render_qweb_pdf(
-                'account.account_invoices', move.ids
-            )[0]
-
-            attachment = self.env['ir.attachment'].create({
-                'name': f'{move.name}.pdf',
-                'type': 'binary',
-                'datas': base64.b64encode(pdf_content),
-                'res_model': 'account.move',
-                'res_id': move.id,
-            })
-
-            mail = self.env['mail.mail'].create({
-                'subject': f'Invoice {move.name}',
-                'body_html': f'Please find attached invoice {move.name}',
-                'email_to': winbooks_email,
-                'attachment_ids': [(4, attachment.id)],
-            })
-            mail.send()
-
+        all_attachments = self.env['ir.attachment'].search([
+            ('res_model', '=', 'account.move'),
+            ('res_id', 'in', self.ids),
+        ])
+        if not all_attachments:
+            raise UserError(_("No attachments."))
+        mail = self.env['mail.mail'].create({
+            'subject': 'All Vendor Bills',
+            'body_html': 'Please check your attachments.',
+            'email_to': winbooks_email,
+            'attachment_ids': [(4, att.id) for att in all_attachments],
+        })
+        mail.send()
         return {
-            'type': 'ir.actions.act_window_close'
+            'type': 'ir.actions.client',
+            'tag': 'display_notification',
+            'params': {
+                'title': 'Success',
+                'message': 'Email sent successfully!',
+                'type': 'success',
+            }
         }
